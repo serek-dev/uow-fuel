2ba31b61f471c315cbfcbc13fa1d3de414e6099c
Sebastian Twaróg <sebastian.twarog@efficioconsulting.com>
2021-06-23 00:38:18 +0200

    Updated core to 1.3.0

---
+++ mock/Mock.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace Orm;
+
+# ugly trick to resolve issue with "not normal" fuel's loader
+
+if (!class_exists('Orm\Model')) {
+
+    class Model implements \ArrayAccess, \Iterator
+    {
+        private $_data;
+
+        public function __construct($data = [], $new = true, $view = null, $cache = true)
+        {
+            $this->_data = $data;
+        }
+
+        public function current()
+        {
+            return current($this->_data);
+        }
+
+        public function next()
+        {
+            return next($this->_data);
+        }
+
+        public function key()
+        {
+            return key($this->_data);
+        }
+
+        public function valid()
+        {
+            return $this->offsetExists(0);
+        }
+
+        public function rewind()
+        {
+
+        }
+
+        public function offsetExists($offset)
+        {
+            return isset($this->_data[$offset]);
+        }
+
+        public function offsetGet($offset)
+        {
+            return $this->_data[$offset];
+        }
+
+        public function offsetSet($offset, $value)
+        {
+            $this->_data[$offset] = $value;
+        }
+
+        public function offsetUnset($offset)
+        {
+            unset($this->_data[$offset]);
+        }
+
+        public function to_array(): array
+        {
+            return $this->_data;
+        }
+
+        public function save($cascade = null, $use_transaction = false)
+        {
+
+        }
+
+        public function __set($name, $value)
+        {
+            $this->offsetSet($name, $value);
+        }
+
+        public function __get($name)
+        {
+            return $this->offsetGet($name);
+        }
+
+        public function get_pk_assoc()
+        {
+            return [];
+        }
+
+        public function columns()
+        {
+            return [];
+        }
+
+        public function is_new()
+        {
+            return true;
+        }
+    }
+}
---
+++ phpstan/bootstrap.php
@@ -0,0 +1,6 @@
+<?php
+
+// Activate the framework class autoloader
+require __DIR__ .'/../vendor/autoload.php';
+require __DIR__ . '/../vendor/core/classes/sanitization.php';
+require __DIR__ . '/../vendor/core/classes/database/query/builder/update.php;
